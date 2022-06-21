328cd57bbbf0cd00ecf6b472a98ef3a0241582b9
russelldnn <russelldnn@gmail.com>
2022-06-21 01:16:48 -0500

    last commit of the night

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let apikey = [];
 let searchBtn = $("#searchBtn");
 let userInput = $("#userInput");
+let History = [];
 
-for (let i = 0; i < localStorage.length; i++){
-    let city = localStorage.getItem(i);
-    let cityList = $("#listedCity")
-    cityList.append("<li>" + city + "</li>");
-}
+
+
+
 
 
 
