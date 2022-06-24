c4e85d50dd505e6634b72b19bf95d0e9d982a925
russelldnn <russelldnn@gmail.com>
2022-06-23 20:27:21 -0500

    added initial variables

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let apikey = [];
+let apikey = 0a183220c7da455b2dfc6ac2a25bb1d1;
 let searchBtn = $("#searchBtn");
-let userInput = $("#userInput");
+let clearBtn = $("#clearBtn");
+let listHistory = $(".listHistory");
 let History = [];
+let cityName = titleCase($("#userInput")[0].value.trim());
+let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=0a183220c7da455b2dfc6ac2a25bb1d1";
+let apiURLOneShot = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=minutely,hourly&units=imperial&appid=0a183220c7da455b2dfc6ac2a25bb1d1";
+
+
+function findInput() {
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+
+            })
+        }
+    })
+}
+
+
 
 
 
